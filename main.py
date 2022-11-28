01901b07e006d5519ec6755109dbe429f0f85a23
KonstantinaStrantzali <konstrantz@gmail.com>
2022-11-28 13:16:33 +0000

    initial commit, loop through the menu

---
+++ main.py
@@ -0,0 +1,45 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+report = {
+    "Water": 100,
+    "Milk": 50,
+    "Coffee": 76,
+    "Money": 2.5,
+
+}
+
+
+choice = input("What would you like? (espresso/latte/cappuccino): ")
+if choice == "report":
+    "Water": 100,
+    "Milk": 50,
+    "Coffee": 76,
+    "Money": 2.5,
+elif choice == "espresso":
+    for item in MENU:
+        print(item["espresso"])
+
+
